7340f1d69dcaa61780ddef601e21caed529d7f42
John Cler <choquejohn6@gmail.com>
2024-05-20 18:48:58 -0400

    Validando formulario

--- flutter_application_1/lib/registrarEquipo.dart
+++ flutter_application_1/lib/registrarEquipo.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class RegistrarEquipo extends StatelessWidget {
-  static const String ROUTE = "/registrar";
+  static String ROUTE = "/registrar";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Guardar"),
+        title: const Text("Guardar"),
       ),
       body: Container(
         child: _FormEquipo(),
@@ -18,31 +18,58 @@ class RegistrarEquipo extends StatelessWidget {
 class _FormEquipo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Column(
-        children: <Widget>[
-          const SizedBox(height: 15),
-          TextFormField(
-            decoration: const InputDecoration(
-                labelText: "Codigo de equipo", border: OutlineInputBorder()),
+    return Container(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 15),
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Tiene que ingresar codigo";
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                    labelText: "Codigo de equipo",
+                    border: OutlineInputBorder()),
+              ),
+              const SizedBox(height: 15),
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Tiene que ingresar titúlo";
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                    labelText: "Titulo", border: OutlineInputBorder()),
+              ),
+              const SizedBox(height: 15),
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Tiene que ingresar fecha";
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                    labelText: "Fecha", border: OutlineInputBorder()),
+              ),
+              const SizedBox(height: 15),
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Tiene que ingresar descripción";
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                    labelText: "Descripción", border: OutlineInputBorder()),
+              )
+            ],
           ),
-          const SizedBox(height: 15),
-          TextFormField(
-            decoration: const InputDecoration(
-                labelText: "Titulo", border: OutlineInputBorder()),
-          ),
-          const SizedBox(height: 15),
-          TextFormField(
-            decoration: const InputDecoration(
-                labelText: "Fecha", border: OutlineInputBorder()),
-          ),
-          const SizedBox(height: 15),
-          TextFormField(
-            decoration: const InputDecoration(
-                labelText: "Descripción", border: OutlineInputBorder()),
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
