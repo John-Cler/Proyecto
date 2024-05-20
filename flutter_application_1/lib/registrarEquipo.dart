ffab0fce69695b2944b01e1218b683bc66fa804c
John Cler <choquejohn6@gmail.com>
2024-05-20 18:31:42 -0400

    Creando formulario formulario

--- flutter_application_1/lib/registrarEquipo.dart
+++ flutter_application_1/lib/registrarEquipo.dart
@@ -18,6 +18,31 @@ class RegistrarEquipo extends StatelessWidget {
 class _FormEquipo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Form(child: Text(""));
+    return Form(
+      child: Column(
+        children: <Widget>[
+          const SizedBox(height: 15),
+          TextFormField(
+            decoration: const InputDecoration(
+                labelText: "Codigo de equipo", border: OutlineInputBorder()),
+          ),
+          const SizedBox(height: 15),
+          TextFormField(
+            decoration: const InputDecoration(
+                labelText: "Titulo", border: OutlineInputBorder()),
+          ),
+          const SizedBox(height: 15),
+          TextFormField(
+            decoration: const InputDecoration(
+                labelText: "Fecha", border: OutlineInputBorder()),
+          ),
+          const SizedBox(height: 15),
+          TextFormField(
+            decoration: const InputDecoration(
+                labelText: "Descripción", border: OutlineInputBorder()),
+          )
+        ],
+      ),
+    );
   }
 }
