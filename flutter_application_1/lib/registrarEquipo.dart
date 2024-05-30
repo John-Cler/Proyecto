93770e938011227cfedaf110f3dbd8f62feed62b
John Cler <choquejohn6@gmail.com>
2024-05-30 00:38:44 -0400

    Modificando registro de equipo

--- flutter_application_1/lib/registrarEquipo.dart
+++ flutter_application_1/lib/registrarEquipo.dart
@@ -9,8 +9,11 @@ class RegistrarEquipo extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Guardar"),
       ),
-      body: Container(
-        child: _FormEquipo(),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(15),
+          child: _FormEquipo(),
+        ),
       ),
     );
   }
@@ -30,135 +33,141 @@ class _FormEquipo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: const EdgeInsets.all(15),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              const SizedBox(height: 15),
-              TextFormField(
-                controller: codeControlller,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Tiene que ingresar codigo";
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                    labelText: "Codigo de equipo",
-                    border: OutlineInputBorder()),
-              ),
-              const SizedBox(height: 15),
-              TextFormField(
-                controller: tipoControlller,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Tiene que ingresar tipo";
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                    labelText: "Tipo", border: OutlineInputBorder()),
-              ),
-              const SizedBox(height: 15),
-              TextFormField(
-                controller: dateControlller,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Tiene que ingresar departamento";
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                    labelText: "Departamento", border: OutlineInputBorder()),
-              ),
-              const SizedBox(height: 15),
-              TextFormField(
-                controller: descripcionControlller,
-                maxLines: 8, //Caclula la cantidad de lineas que debe ingresar
-                maxLength:
-                    100, //calcula la cantidad maxima que debe ingresar en el text
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Tiene que ingresar descripción";
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                    labelText: "Descripción", border: OutlineInputBorder()),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              TextFormField(
-                controller: activoControlller,
-                maxLines: 8,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Ingresae el Número de activo";
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                    labelText: "Nro Activo", border: OutlineInputBorder()),
-              ),
-              const SizedBox(height: 15),
-              TextFormField(
-                controller: nseriControlller,
-                maxLines: 8,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Ingresar de serie";
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                  labelText: "Nro de serie",
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              TextFormField(
-                controller: marcaControlller,
-                maxLines: 8,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Ingresar marca del equipo";
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                  labelText: "Marca de equipo",
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              TextFormField(
-                controller: marcaControlller,
-                maxLines: 8,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Ingresar modelo de equipo";
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                  labelText: "Marca de equipo",
-                ),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      logger.i("Validar" + codeControlller.text);
-                      logger.i("Validar" + tipoControlller.text);
-                    }
-                  },
-                  child: const Text('Button'))
-            ],
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const SizedBox(height: 15),
+          TextFormField(
+            controller: codeControlller,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "Tiene que ingresar codigo";
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              labelText: "Codigo de equipo",
+              border: OutlineInputBorder(),
+            ),
           ),
-        ));
+          const SizedBox(height: 15),
+          TextFormField(
+            controller: tipoControlller,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "Tiene que ingresar tipo";
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              labelText: "Tipo",
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 15),
+          TextFormField(
+            controller: dateControlller,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "Tiene que ingresar departamento";
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              labelText: "Departamento",
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 15),
+          TextFormField(
+            controller: descripcionControlller,
+            maxLines: 8, // Define la cantidad de lineas que debe ingresar
+            maxLength:
+                100, // Calcula la cantidad máxima que debe ingresar en el texto
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "Tiene que ingresar descripción";
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              labelText: "Descripción",
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 15),
+          TextFormField(
+            controller: activoControlller,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "Ingresar el Número de activo";
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              labelText: "Nro Activo",
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 15),
+          TextFormField(
+            controller: nseriControlller,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "Ingresar número de serie";
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              labelText: "Nro de serie",
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 15),
+          TextFormField(
+            controller: marcaControlller,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "Ingresar marca del equipo";
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              labelText: "Marca de equipo",
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 15),
+          TextFormField(
+            controller: modeloControlller,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "Ingresar modelo de equipo";
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              labelText: "Modelo de equipo",
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 15),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  logger.i("Validar: ${codeControlller.text}");
+                  logger.i("Validar: ${tipoControlller.text}");
+                  // Se pueden agregar más validaciones aquí si es necesario
+                }
+              },
+              child: const Text('Button'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
