9b8355855f46c5a0d0453dc5a0f49b1b9a59f80f
John Cler <choquejohn6@gmail.com>
2024-05-31 02:00:05 -0400

    Modificando Menu creando botones de sub menu

--- flutter_application_1/lib/menu.dart
+++ flutter_application_1/lib/menu.dart
@@ -35,7 +35,111 @@ class Menu extends StatelessWidget {
           child: Column(
             children: [
               Container(
+                margin: const EdgeInsets.all(20),
                 child: Image.asset("lib/assets/appbar.png"),
+              ),
+              const Text(
+                "Administrador",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 30),
+                padding: const EdgeInsets.all(20),
+                width: double.infinity,
+                color: Colors.blue[700],
+                alignment: Alignment.center,
+                child: const Text(
+                  "Home",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 2),
+                padding: const EdgeInsets.all(20),
+                width: double.infinity,
+                color: Colors.blue[400],
+                alignment: Alignment.center,
+                child: const Text(
+                  "Registrar",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                      color: Colors.white),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 2),
+                padding: const EdgeInsets.all(20),
+                width: double.infinity,
+                color: Colors.blue[400],
+                alignment: Alignment.center,
+                child: const Text(
+                  "Mantenimiento",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                      color: Colors.white),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 2),
+                padding: const EdgeInsets.all(20),
+                width: double.infinity,
+                color: Colors.blue[400],
+                alignment: Alignment.center,
+                child: const Text(
+                  "Reporte",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                      color: Colors.white),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 2),
+                padding: const EdgeInsets.all(20),
+                width: double.infinity,
+                color: Colors.blue[400],
+                alignment: Alignment.center,
+                child: const Text(
+                  "Seguimiento",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                      color: Colors.white),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 2),
+                padding: const EdgeInsets.all(20),
+                width: double.infinity,
+                color: Colors.blue[400],
+                alignment: Alignment.center,
+                child: const Text(
+                  "Asignación",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                      color: Colors.white),
+                ),
+              ),
+              Expanded(child: Container()),
+              Container(
+                margin: const EdgeInsets.only(top: 2),
+                padding: const EdgeInsets.all(20),
+                width: double.infinity,
+                color: const Color.fromARGB(255, 180, 32, 21),
+                alignment: Alignment.center,
+                child: const Text(
+                  "Sign outt",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                      color: Colors.white),
+                ),
               )
             ],
           ),
