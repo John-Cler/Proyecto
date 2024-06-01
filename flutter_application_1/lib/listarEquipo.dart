6e17bba4b2ad9a6502c8940a8d54120a20185400
John Cler <choquejohn6@gmail.com>
2024-06-01 18:37:43 -0400

    Creando modulo de listar registro

--- flutter_application_1/lib/listarEquipo.dart
+++ flutter_application_1/lib/listarEquipo.dart
@@ -12,19 +12,21 @@ class ListarRegistroEquipo extends StatefulWidget {
 
 class _ListarRegistroEquipoState extends State<ListarRegistroEquipo> {
   final List<Map<String, String>> _equipos = [];
-
+  //Funcion de Guardar los registros
   void _addEquipo(Map<String, String> equipo) {
     setState(() {
       _equipos.add(equipo);
     });
   }
 
+  //Funcion de Editar los registros
   void _editEquipo(int index, Map<String, String> equipo) {
     setState(() {
       _equipos[index] = equipo;
     });
   }
 
+  //Funcion de eliminar los registros
   void _deleteEquipo(int index) {
     setState(() {
       _equipos.removeAt(index);
@@ -34,6 +36,7 @@ class _ListarRegistroEquipoState extends State<ListarRegistroEquipo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //Incio de Buton flotante de nuevo registro
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () async {
@@ -46,9 +49,9 @@ class _ListarRegistroEquipoState extends State<ListarRegistroEquipo> {
             _addEquipo(result as Map<String, String>);
           }
         },
-      ),
+      ), //Fin de Butoon Flotante para agregar nuevo registro
       appBar: AppBar(
-        title: const Text("Lista de equipos por John clever asi nomas"),
+        title: const Text("Lista de equipos"),
       ),
       body: ListView.builder(
         itemCount: _equipos.length,
@@ -62,7 +65,7 @@ class _ListarRegistroEquipoState extends State<ListarRegistroEquipo> {
               mainAxisSize: MainAxisSize.min,
               children: [
                 IconButton(
-                  icon: Icon(Icons.edit),
+                  icon: const Icon(Icons.edit),
                   onPressed: () async {
                     final result = await Navigator.pushNamed(
                       context,
@@ -88,229 +91,3 @@ class _ListarRegistroEquipoState extends State<ListarRegistroEquipo> {
     );
   }
 }
-
-
-// import 'package:flutter/material.dart';
-// import 'package:logger/logger.dart';
-// import 'package:intl/intl.dart'; // Para manejar el formato de fecha
-
-// class RegistrarEquipo extends StatefulWidget {
-//   static String ROUTE = "/registrarEquipo";
-
-//   @override
-//   _RegistrarEquipoState createState() => _RegistrarEquipoState();
-// }
-
-// class _RegistrarEquipoState extends State<RegistrarEquipo> {
-//   final _formKey = GlobalKey<FormState>();
-//   final logger = Logger();
-//   final codeController = TextEditingController();
-//   String tipo = 'Computadora';
-//   final dateController = TextEditingController();
-//   final descripcionController = TextEditingController();
-//   final activoController = TextEditingController();
-//   final nseriController = TextEditingController();
-//   final marcaController = TextEditingController();
-//   final modeloController = TextEditingController();
-
-//   Map<String, String>? _currentEquipo;
-
-//   @override
-//   void didChangeDependencies() {
-//     super.didChangeDependencies();
-//     final equipo = ModalRoute.of(context)!.settings.arguments as Map<String, String>?;
-//     if (equipo != null) {
-//       _currentEquipo = equipo;
-//       codeController.text = equipo["codigo"]!;
-//       tipo = equipo["tipo"]!;
-//       dateController.text = equipo["fecha"]!;
-//       descripcionController.text = equipo["descripcion"]!;
-//       activoController.text = equipo["activo"]!;
-//       nseriController.text = equipo["serie"]!;
-//       marcaController.text = equipo["marca"]!;
-//       modeloController.text = equipo["modelo"]!;
-//     }
-//   }
-
-//   _pickDate() async {
-//     DateTime? picked = await showDatePicker(
-//       context: context,
-//       initialDate: DateTime.now(),
-//       firstDate: DateTime(2000),
-//       lastDate: DateTime(2101),
-//     );
-
-//     if (picked != null) {
-//       setState(() {
-//         dateController.text = DateFormat('yyyy-MM-dd').format(picked);
-//       });
-//     }
-//   }
-
-//   _saveForm() {
-//     if (_formKey.currentState!.validate()) {
-//       final equipo = {
-//         "codigo": codeController.text,
-//         "tipo": tipo,
-//         "fecha": dateController.text,
-//         "descripcion": descripcionController.text,
-//         "activo": activoController.text,
-//         "serie": nseriController.text,
-//         "marca": marcaController.text,
-//         "modelo": modeloController.text,
-//       };
-
-//       Navigator.pop(context, equipo);
-
-//       logger.i("Equipo registrado: $equipo");
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text("Registrar Equipo"),
-//       ),
-//       body: SingleChildScrollView(
-//         child: Container(
-//           padding: const EdgeInsets.all(15),
-//           child: Form(
-//             key: _formKey,
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: <Widget>[
-//                 const SizedBox(height: 15),
-//                 TextFormField(
-//                   controller: codeController,
-//                   validator: (value) {
-//                     if (value!.isEmpty) {
-//                       return "Tiene que ingresar codigo";
-//                     }
-//                     return null;
-//                   },
-//                   decoration: const InputDecoration(
-//                     labelText: "Codigo de equipo",
-//                     border: OutlineInputBorder(),
-//                   ),
-//                 ),
-//                 const SizedBox(height: 15),
-//                 DropdownButtonFormField<String>(
-//                   value: tipo,
-//                   items: ['Computadora', 'Scanner', 'Impresora', 'Otros']
-//                       .map((label) => DropdownMenuItem(
-//                             child: Text(label),
-//                             value: label,
-//                           ))
-//                       .toList(),
-//                   onChanged: (value) {
-//                     setState(() {
-//                       tipo = value!;
-//                     });
-//                   },
-//                   decoration: const InputDecoration(
-//                     labelText: "Tipo de equipo",
-//                     border: OutlineInputBorder(),
-//                   ),
-//                 ),
-//                 const SizedBox(height: 15),
-//                 TextFormField(
-//                   controller: dateController,
-//                   readOnly: true,
-//                   onTap: _pickDate,
-//                   validator: (value) {
-//                     if (value!.isEmpty) {
-//                       return "Tiene que ingresar fecha";
-//                     }
-//                     return null;
-//                   },
-//                   decoration: const InputDecoration(
-//                     labelText: "Fecha",
-//                     border: OutlineInputBorder(),
-//                   ),
-//                 ),
-//                 const SizedBox(height: 15),
-//                 TextFormField(
-//                   controller: descripcionController,
-//                   validator: (value) {
-//                     if (value!.isEmpty) {
-//                       return "Tiene que ingresar descripción";
-//                     }
-//                     return null;
-//                   },
-//                   decoration: const InputDecoration(
-//                     labelText: "Descripción",
-//                     border: OutlineInputBorder(),
-//                   ),
-//                 ),
-//                 const SizedBox(height: 15),
-//                 TextFormField(
-//                   controller: activoController,
-//                   validator: (value) {
-//                     if (value!.isEmpty) {
-//                       return "Ingresar el Número de activo";
-//                     }
-//                     return null;
-//                   },
-//                   decoration: const InputDecoration(
-//                     labelText: "Nro Activo",
-//                     border: OutlineInputBorder(),
-//                   ),
-//                 ),
-//                 const SizedBox(height: 15),
-//                 TextFormField(
-//                   controller: nseriController,
-//                   validator: (value) {
-//                     if (value!.isEmpty) {
-//                       return "Ingresar número de serie";
-//                     }
-//                     return null;
-//                   },
-//                   decoration: const InputDecoration(
-//                     labelText: "Nro de serie",
-//                     border: OutlineInputBorder(),
-//                   ),
-//                 ),
-//                 const SizedBox(height: 15),
-//                 TextFormField(
-//                   controller: marcaController,
-//                   validator: (value) {
-//                     if (value!.isEmpty) {
-//                       return "Ingresar marca del equipo";
-//                     }
-//                     return null;
-//                   },
-//                   decoration: const InputDecoration(
-//                     labelText: "Marca de equipo",
-//                     border: OutlineInputBorder(),
-//                   ),
-//                 ),
-//                 const SizedBox(height: 15),
-//                 TextFormField(
-//                   controller: modeloController,
-//                   validator: (value) {
-//                     if (value!.isEmpty) {
-//                       return "Ingresar modelo de equipo";
-//                     }
-//                     return null;
-//                   },
-//                   decoration: const InputDecoration(
-//                     labelText: "Modelo de equipo",
-//                     border: OutlineInputBorder(),
-//                   ),
-//                 ),
-//                 const SizedBox(height: 15),
-//                 Center(
-//                   child: ElevatedButton(
-//                     onPressed: _saveForm,
-//                     child: const Text('Guardar'),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
