cd3f42a65a508d5a812d999854f557f01f40d332
John Cler <choquejohn6@gmail.com>
2024-06-05 02:20:51 -0400

    Prueba de conexion con el backend

---
+++ flutter_application_1/lib/registrarBloque.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class RegisterBloque extends StatefulWidget {
+  static const String ROUTE = "/registrarBloque";
+  @override
+  _RegisterBlockScreenState createState() => _RegisterBlockScreenState();
+}
+
+class _RegisterBlockScreenState extends State<RegisterBloque> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registrar Bloque'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _nameController,
+                decoration:
+                    const InputDecoration(labelText: 'Nombre del Bloque'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingrese el nombre del bloque';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _registerBlock();
+                  }
+                },
+                child: const Text('Registrar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _registerBlock() async {
+    final response = await http.post(
+      Uri.parse('http://localhost:8080/api/v1/bloques/guardar'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'nombre': _nameController.text,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Bloque registrado con éxito')),
+      );
+      _nameController.clear();
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error al registrar el bloque')),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+}
