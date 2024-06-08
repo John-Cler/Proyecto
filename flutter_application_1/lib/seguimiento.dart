76b2524d8b088531acb0e582a92de279222922da
John Cler <choquejohn6@gmail.com>
2024-06-07 20:13:52 -0400

    pantalla de seguimiento

---
+++ flutter_application_1/lib/seguimiento.dart
@@ -0,0 +1,17 @@
+// import 'package:flutter/material.dart';
+
+// class Seguimiento extends StatefulWidget {
+//   static String ROUTE = "/seguimiento";
+//   @override
+//   _SeguimientoScreen createstate() => _SeguimientoScreen();
+// }
+
+// class _SeguimientoScreen extends State<Seguimiento> {
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return Scaffold(
+//       appBar: AppBar("Seguimiento de"),
+//     );
+//   }
+// }
