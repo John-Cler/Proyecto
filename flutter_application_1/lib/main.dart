9fd8b98227b6de79fa15870898089e72eee3bb64
John Cler <choquejohn6@gmail.com>
2024-06-13 22:15:37 -0400

    modificando main de material fluter

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/addMantenimiento.dart';
+import 'package:flutter_application_1/Mantenimiento/addMantenimiento.dart';
+import 'package:flutter_application_1/Mantenimiento/solicitudMantenimiento.dart';
 import 'package:flutter_application_1/asignarEquipo.dart';
-import 'package:flutter_application_1/detalleMantenimiento.dart';
-import 'package:flutter_application_1/listaMantenimiento.dart';
+import 'package:flutter_application_1/Mantenimiento/detalleMantenimiento.dart';
+import 'package:flutter_application_1/Mantenimiento/listaMantenimiento.dart';
 import 'package:flutter_application_1/listarEquipo.dart';
 import 'package:flutter_application_1/listarPersona.dart';
-import 'package:flutter_application_1/login.dart';
+import 'package:flutter_application_1/Login/login.dart';
 import 'package:flutter_application_1/menu.dart';
 import 'package:flutter_application_1/registrarBloque.dart';
 import 'package:flutter_application_1/registrarDepartamento.dart';
-import 'package:flutter_application_1/registrarEquipo.dart';
+import 'package:flutter_application_1/registrarEquipo/registrarEquipo.dart';
 import 'package:flutter_application_1/registrarPersona.dart';
 import 'package:flutter_application_1/reporte.dart';
+import 'listadoUsuarios.dart';
+import 'registrarUsuario.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,19 +31,22 @@ class MyApp extends StatelessWidget {
       routes: {
         Login.ROUTE: (_) => const Login(),
         Menu.ROUTE: (_) => const Menu(),
-        ListarRegistroEquipo.ROUTE: (_) => const ListarRegistroEquipo(),
-        ListarPersona.ROUTE: (_) => const ListarPersona(),
-        ListaMantenimiento.ROUTE: (_) => ListaMantenimiento(),
-        DetalleMantenimiento.ROUTE: (_) => DetalleMantenimiento(),
-        RegisterBloque.ROUTE: (_) => const RegisterBloque(),
-        RegistrarDepartamento.ROUTE: (_) => RegistrarDepartamento(),
-        AddMantenimiento.ROUTE: (_) => AddMantenimiento(
-              onAddRecord: (MaintenanceRecord) {},
-            ),
-        Reporte.ROUTE: (_) => Reporte(),
-        RegistrarEquipo.ROUTE: (_) => const RegistrarEquipo(),
-        RegistrarPersona.ROUTE: (_) => const RegistrarPersona(),
-        AsignarEquipo.ROUTE: (_) => AsignarEquipo(),
+        ListarRegistroEquipo.route: (_) => const ListarRegistroEquipo(),
+        ListarPersona.route: (_) => const ListarPersona(),
+        ListaMantenimiento.route: (_) => const ListaMantenimiento(),
+        DetalleMantenimiento.route: (_) => DetalleMantenimiento(),
+        RegisterBloque.route: (_) => const RegisterBloque(),
+        RegistrarDepartamento.route: (_) => RegistrarDepartamento(),
+        AddMantenimiento.route: (_) =>
+            AddMantenimiento(onAddRecord: (record) {}),
+        SolicitudMantenimiento.route: (context) =>
+            const SolicitudMantenimiento(),
+        Reporte.route: (_) => Reporte(),
+        RegistrarEquipo.route: (_) => const RegistrarEquipo(),
+        RegistrarPersona.route: (_) => const RegistrarPersona(),
+        AsignarEquipo.route: (_) => AsignarEquipo(),
+        ListadoUsuarios.route: (context) => ListadoUsuarios(),
+        RegistrarUsuario.route: (context) => const RegistrarUsuario(),
       },
     );
   }
